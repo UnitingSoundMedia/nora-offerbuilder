7209b7a1db3a119020d9a282e0701e7b619186c1
UnitingSoundMedia <j.wolff@unitingsound.media>
2025-05-23 04:53:06 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import OfferBuilder from './OfferBuilder';
+
+function App() {
+  return (
+    <div className="App">
+      <OfferBuilder />
+    </div>
+  );
+}
+
+export default App;
